144390df29c734c591ac6d23e84c67dcebd73eb9
Tanel Põld <tanel@brightside.ee>
2018-04-11 12:26:54 +0800

    Add comments for constant editor

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,5 +1,11 @@
+# customsubcategory=personnel=Personnel
+# cat=Personnel/personnel; type=string; label= Styles file
 personnel.stylesPath = EXT:personnel/Resources/Public/Styles/personnel.css
+# cat=Personnel/personnel; type=string; label= Templates root path
 personnel.templateRootPaths = EXT:personnel/Resources/Private/Templates/
+# cat=Personnel/personnel; type=string; label= Partials root path
 personnel.partialRootPaths = EXT:personnel/Resources/Private/Partials/
+# cat=Personnel/personnel; type=string; label= vCard template root path
 personnel.vCard.templateRootPaths = EXT:personnel/Resources/Private/Templates/
+# cat=Personnel/personnel; type=string; label= Company name in vCard
 personnel.vCard.CompanyName = Example Company Ltd.

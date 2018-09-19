a57993fbff888db9d4edbba67eedf05047a44b0f
Tanel Põld <tanel@brightside.ee>
2018-09-19 14:39:30 +0300

    following best practices

--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
-
+(function () {
   \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_personnel_domain_model_person');
   \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tx_personnel_domain_model_person');
+})();

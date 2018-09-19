f326a34eaf709b9cd3dd31e186a63a78faeb06bd
Tanel Põld <tanel@brightside.ee>
2018-09-19 19:00:35 +0300

    Clean up

--- Classes/Hooks/ContentPostProcessor.php
+++ Classes/Hooks/ContentPostProcessor.php
@@ -5,31 +5,28 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class ContentPostProcessor
 {
-    /**
-     * @param $funcRef
-     */
-    public function render($params) {
-        /** @var \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $feobj */
-        $feobj = &$params['pObj'];
-
-        if($GLOBALS['TSFE']->type == 888){
-            $personId = (int) GeneralUtility::_GET('person');
-            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_personnel_domain_model_person');
-            $result = $queryBuilder
-              ->select('*')
-              ->from('tx_personnel_domain_model_person')
-              ->where($queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($personId, \PDO::PARAM_INT)))
-              ->execute();
-            $rows = $result->fetchAll();
-
-            $vcfFilename = 'person.vcf';
-            if(is_array($rows)){
-              foreach ($rows as $row => $col) {
-                $vcfFilename = $col['firstname'] . '_' . $col['lastname'] . '.vcf';
-              }
-            }
-            header('Content-type:text/x-vCard');
-            header('Content-Disposition: attachment; ; filename="'.$vcfFilename.'"');
+  /**
+   * @param $funcRef
+   */
+  public function render($params) {
+    /** @var \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $feobj */
+    $feobj = &$params['pObj'];
+    if($GLOBALS['TSFE']->type == 888){
+      $personId = (int) GeneralUtility::_GET('person');
+      $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_personnel_domain_model_person');
+      $statement = $queryBuilder
+        ->select('*')
+        ->from('tx_personnel_domain_model_person')
+        ->where($queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($personId, \PDO::PARAM_INT)))
+        ->execute();
+        $vcfFilename = 'person.vcf';
+        while ($row = $statement->fetch()) {
+          if ($row['firstname'] || $row['lastname']) {
+            $vcfFilename = $row['firstname'] . '_' . $row['lastname'] . '.vcf';
+          }
         }
+      header('Content-type:text/x-vCard');
+      header('Content-Disposition: attachment; ; filename="'.$vcfFilename.'"');
     }
+  }
 }

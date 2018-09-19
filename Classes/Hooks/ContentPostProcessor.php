f2f90c4aa8cc6da50ef2ad012511171fdd490775
Tanel Põld <tanel@brightside.ee>
2018-09-19 14:34:12 +0300

    DB to doctrine

--- Classes/Hooks/ContentPostProcessor.php
+++ Classes/Hooks/ContentPostProcessor.php
@@ -1,5 +1,6 @@
 <?php
 namespace Brightside\Personnel\Hooks;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class ContentPostProcessor
@@ -8,24 +9,25 @@ class ContentPostProcessor
      * @param $funcRef
      */
     public function render($params) {
-
         /** @var \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $feobj */
         $feobj = &$params['pObj'];
 
         if($GLOBALS['TSFE']->type == 888){
             $personId = (int) GeneralUtility::_GET('person');
-
-            $person = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow(
-                'lastname,firstname',
-                'tx_personnel_domain_model_person',
-                'uid='. $personId
-            );
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_personnel_domain_model_person');
+            $result = $queryBuilder
+              ->select('*')
+              ->from('tx_personnel_domain_model_person')
+              ->where($queryBuilder->expr()->eq('uid', $queryBuilder->createNamedParameter($personId, \PDO::PARAM_INT)))
+              ->execute();
+            $rows = $result->fetchAll();
 
             $vcfFilename = 'person.vcf';
-            if(is_array($person)){
-                $vcfFilename = $person['firstname'] . '_' . $person['lastname'] . '.vcf';
+            if(is_array($rows)){
+              foreach ($rows as $row => $col) {
+                $vcfFilename = $col['firstname'] . '_' . $col['lastname'] . '.vcf';
+              }
             }
-
             header('Content-type:text/x-vCard');
             header('Content-Disposition: attachment; ; filename="'.$vcfFilename.'"');
         }

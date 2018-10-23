effe4c92edb46c2bdeffe3064465ed07e31f1219
Tanel Põld <tanel@brightside.ee>
2018-10-23 12:39:11 +0800

    Cleanup

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,4 @@ defined('TYPO3_MODE') or die();
 (function () {
   \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_personnel_domain_model_person');
   \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToInsertRecords('tx_personnel_domain_model_person');
-})();
+});

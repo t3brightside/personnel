7d5d6f5f6fcbf788b7ce616702a219874c13c458
Tanel Põld <tanel@brightside.ee>
2018-10-23 12:38:48 +0800

    Add function

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 defined('TYPO3_MODE') or die();
-
+(function () {
   \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:personnel/Configuration/PageTS/setup.ts">');
 
   $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
@@ -14,3 +14,4 @@ defined('TYPO3_MODE') or die();
       \Brightside\Personnel\Hooks\ContentPostProcessor::class . '->render';
 
   $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['personnel'] = \Brightside\Personnel\Hooks\PageLayoutView\PersonnelContentElementPreviewRenderer::class;
+});

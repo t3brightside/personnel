5ee423dd2f058db47dd4f41c170117c6d49a33e7
Tanel Põld <tanel@brightside.ee>
2018-10-23 12:27:50 +0800

    Add links to page module preview content

--- Classes/Hooks/PageLayoutView/PersonnelContentElementPreviewRenderer.php
+++ Classes/Hooks/PageLayoutView/PersonnelContentElementPreviewRenderer.php
@@ -20,32 +20,29 @@ class PersonnelContentElementPreviewRenderer implements PageLayoutViewDrawItemHo
 	 public function preProcess(PageLayoutView &$parentObject, &$drawItem, &$headerContent, &$itemContent, array &$row) {
 		if ($row['CType'] === 'personnel_frompages' || $row['CType'] === 'personnel_selected') {
 			if ($row['CType'] === 'personnel_frompages') {
-        $itemContent .= '<span>Persons from pages</span>';
+				$itemContent = $parentObject->linkEditContent('<span style="display: block; margin-top: 0.3em;">Persons from page: '. $row['pages'] .'</span>', $row);
 			}
 			if ($row['CType'] === 'personnel_selected') {
-        $itemContent .= '<span>Selected persons</span>';
+        $itemContent = $parentObject->linkEditContent('<span style="display: block; margin-top: 0.3em;">Selected persons: '. $row['pages'] .'</span>', $row);
 			}
 			$itemContent .= '<ul style="margin: 0; padding: 0.2em 1.4em;">';
 			if ($row['tx_personnel_orderby']) {
         $itemContent .= '<li>Order by: ' . $parentObject->linkEditContent($parentObject->renderText($row['tx_personnel_orderby']), $row) . '</li>';
 			}
 			if ($row['tx_personnel_images'] == 1) {
-        $itemContent .= '<li>Images: disabled</li>';
+				$itemContent .= '<li>' . $parentObject->linkEditContent('Images: disabled', $row) . '</li>';
 			}
 			if ($row['tx_personnel_vcard'] == 1) {
-        $itemContent .= '<li>vCard: disabled</li>';
+				$itemContent .= '<li>' . $parentObject->linkEditContent('vCard: disabled', $row) . '</li>';
 			}
 			if ($row['tx_personnel_information'] == 1) {
-        $itemContent .= '<li>Information: disabled</li>';
+				$itemContent .= '<li>' . $parentObject->linkEditContent('Information: disabled', $row) . '</li>';
 			}
 			if ($row['tx_personnel_paginate'] == 1) {
-        $itemContent .= '<li>Pagination: enabled</li>';
-			}
-			if ($row['tx_personnel_paginateitems'] > 1 && $row['tx_personnel_paginate'] == 1) {
-				$itemContent .= '<li>Items per page: ' . $parentObject->linkEditContent($parentObject->renderText($row['tx_personnel_paginateitems']), $row) . '</li>';
+				$itemContent .= '<li>' . $parentObject->linkEditContent('Pagination: enabled', $row) . '</li>';
 			}
 			if ($row['tx_personnel_paginateitems'] == 0 && $row['tx_personnel_paginate'] == 1) {
-				$itemContent .= '<li>Items per page: TypoScript';
+				$itemContent .= '<li>' . $parentObject->linkEditContent($parentObject->renderText('Items per page: ' . $row['tx_pagelist_paginateitems']), $row) . '</li>';
 			}
 			$itemContent .= '</ul>';
 			$drawItem = FALSE;

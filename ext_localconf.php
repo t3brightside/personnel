86b9ac69217efaba43c1698e6f57ef1f29661073
Tanel Põld <tanel@brightside.ee>
2018-10-23 12:40:48 +0800

    Clean up

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,13 +5,13 @@ defined('TYPO3_MODE') or die();
 
   $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
   $iconRegistry->registerIcon(
-      'mimetypes-x-content-personnel',
-      \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-      ['source' => 'EXT:personnel/Resources/Public/Images/Icons/mimetypes-x-content-personnel.svg']
+    'mimetypes-x-content-personnel',
+    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+    ['source' => 'EXT:personnel/Resources/Public/Images/Icons/mimetypes-x-content-personnel.svg']
   );
 
   $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] =
-      \Brightside\Personnel\Hooks\ContentPostProcessor::class . '->render';
+    \Brightside\Personnel\Hooks\ContentPostProcessor::class . '->render';
 
   $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['personnel'] = \Brightside\Personnel\Hooks\PageLayoutView\PersonnelContentElementPreviewRenderer::class;
 })();

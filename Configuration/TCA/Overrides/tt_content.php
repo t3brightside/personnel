5a29765aa78d30e51a752c81e5e300882632c0d3
Tanel Põld <tanel@brightside.ee>
2019-08-22 08:51:47 +0300

    Clean up

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -31,7 +31,7 @@
 		)
 	);
 	call_user_func(
-    function () {
+    	function () {
 			$tempColumns = array(
 		    'tx_personnel' => [
 					'exclude' => 1,
@@ -64,9 +64,9 @@
 					  'renderType' => 'selectSingle',
 						'default' => 0,
 						'items' => array(
-			      	array('Manual (default)', '0'),
+			      			array('Manual (default)', '0'),
 							array('By the sort order', 'sorting ASC'),
-				    	array('Lastname (a → z)', 'lastname ASC'),
+				    		array('Lastname (a → z)', 'lastname ASC'),
 							array('Lastname (z → a)', 'lastname DESC'),
 							array('First name (a → z)', 'firstname ASC'),
 							array('First name (z → a)', 'firstname DESC'),
@@ -198,30 +198,25 @@
 			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns);
 			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumnsCheck);
 
-		// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tt_content', 'EXT:personnel/Resources/Private/Language/locallang_db.xml');
-
 			$GLOBALS['TCA']['tt_content']['types']['personnel_selected'] = array(
 				'showitem' => '
 					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-		        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-		        --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
-		        tx_personnel,
-		        --palette--;;personnelSettings,
-		      --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
-		          --palette--;;language,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-		          --palette--;;hidden,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
-		          categories,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
-		          rowDescription,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
-		      --div--;LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xlf:gridElements, tx_gridelements_container, tx_gridelements_columns
-		    '
+			        	--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+			        	--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,tx_personnel,
+			        	--palette--;;personnelSettings,
+					--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+						--palette--;;language,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+						--palette--;;hidden,
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,categories,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,rowDescription,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+					--div--;LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xlf:gridElements, tx_gridelements_container, tx_gridelements_columns
+		    	'
 			);
 
 			$GLOBALS['TCA']['tt_content']['palettes']['personnelSettings']['showitem'] = '
@@ -237,27 +232,24 @@
 			$GLOBALS['TCA']['tt_content']['types']['personnel_frompages'] = array(
 				'showitem' => '
 					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
-		          pages,
-		          --palette--;;personnelSettings,
-		      --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
-		      --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.accessibility,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.menu_accessibility;menu_accessibility,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
-		          --palette--;;language,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-		          --palette--;;hidden,
-		          --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
-		          categories,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
-		          rowDescription,
-		      --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
-		      --div--;LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xlf:gridElements, tx_gridelements_container, tx_gridelements_columns
-		    '
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,pages,
+						--palette--;;personnelSettings,
+					--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,
+					--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.accessibility,
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.menu_accessibility;menu_accessibility,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+						--palette--;;language,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+						--palette--;;hidden,
+						--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,categories,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,rowDescription,
+					--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+					--div--;LLL:EXT:gridelements/Resources/Private/Language/locallang_db.xlf:gridElements, tx_gridelements_container, tx_gridelements_columns
+		    	'
 			);
 
 			$GLOBALS['TCA']['tt_content']['palettes']['personnelSettings']['showitem'] = '
@@ -270,4 +262,4 @@
 				tx_personnel_paginateitems,
 			';
 		}
-  );
+	);

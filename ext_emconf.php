8258864cee1630f4e8641034746bec5381ef1075
Tanel Põld <tanel@brightside.ee>
2020-05-18 19:27:37 +0800

    clean up

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,37 +1,35 @@
 <?php
-defined('TYPO3_MODE') or die();
-
-	$EM_CONF[$_EXTKEY] = array (
-		'title' => 'Personnel',
-		'description' => 'Personnel lists with vCard download.',
-		'category' => 'fe',
-		'version' => '2.4.0',
-		'state' => 'stable',
-		'uploadfolder' => false,
-		'createDirs' => '',
-		'clearcacheonload' => true,
-		'author' => 'Tanel Põld, Nikolay Orlenko',
-		'author_email' => 'tanel@brightside.ee',
-		'author_company' => 'Brightside OÜ / t3brightside.com',
-		'constraints' =>
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Personnel',
+	'description' => 'Personnel lists with vCard download.',
+	'category' => 'fe',
+	'version' => '2.4.0',
+	'state' => 'stable',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Tanel Põld, Nikolay Orlenko',
+	'author_email' => 'tanel@brightside.ee',
+	'author_company' => 'Brightside OÜ / t3brightside.com',
+	'constraints' =>
+	array (
+		'depends' =>
 		array (
-			'depends' =>
-			array (
-				'typo3' => '9.5.0 - 10.4.99',
-				'fluid_styled_content' => '',
-			),
-			'conflicts' =>
-			array (
-			),
-			'suggests' =>
-			array (
-			),
+			'typo3' => '9.5.0 - 10.4.99',
+			'fluid_styled_content' => '',
 		),
-		'autoload' =>
+		'conflicts' =>
 		array (
-			'classmap' =>
-			array (
-				0 => 'Classes',
-			),
 		),
-	);
+		'suggests' =>
+		array (
+		),
+	),
+	'autoload' =>
+	array (
+		'classmap' =>
+		array (
+			0 => 'Classes',
+		),
+	),
+);

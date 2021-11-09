4a9f2f91891e3abe69aef583a45bb54236b9cc03
Tanel Põld <tanel@brightside.ee>
2021-11-09 09:55:05 +0200

    Clean Up

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,20 +9,14 @@ $EM_CONF[$_EXTKEY] = array (
 	'author' => 'Tanel Põld, Nikolay Orlenko',
 	'author_email' => 'tanel@brightside.ee',
 	'author_company' => 'Brightside OÜ / t3brightside.com',
-	'constraints' =>
-	array (
-		'depends' =>
-		array (
+	'constraints' => [
+		'depends' => [
 			'typo3' => '11.5.0 - 11.5.99',
 			'fluid_styled_content' => '11.5.0 - 11.5.99',
 			'paginatedprocessors' => '1.0.0 - 1.0.99',
-		),
-	),
-	'autoload' =>
-	array (
-		'classmap' =>
-		array (
-			0 => 'Classes',
-		),
-	),
-);
+		],
+		'autoload' => [
+        'classmap' => ['Classes'],
+    ],
+	],
+];

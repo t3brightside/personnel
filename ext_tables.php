ea26d261cf281cfa352b93b69507d3a7f97ff4f3
Christoph Lehmann <lehmann@networkteam.com>
2023-02-17 22:33:05 +0100

    Move ExtensionManagementUtility::addToInsertRecords()
    According to phpdoc it should not be in ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -4,5 +4,3 @@ defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 ExtensionManagementUtility::allowTableOnStandardPages('tx_personnel_domain_model_person');
-ExtensionManagementUtility::addToInsertRecords('tx_personnel_domain_model_person');
-

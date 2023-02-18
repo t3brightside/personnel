617c582832e64bb1f1e3897c1d1b43ccaad1ebfe
Tanel Põld <tanel@brightside.ee>
2023-02-18 17:34:48 +0200

    [TASK] allowTableOnStandardPages only for TYPO3 11

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,13 @@
 <?php
-defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
-
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
-ExtensionManagementUtility::allowTableOnStandardPages('tx_personnel_domain_model_person');
+defined('TYPO3') or die();
+
+(function () {
+    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+    if ($versionInformation->getMajorVersion() < 12) {
+        ExtensionManagementUtility::allowTableOnStandardPages('tx_personnel_domain_model_person');
+    }
+})();

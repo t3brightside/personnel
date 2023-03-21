9c20d54e91769ca71f427d912e28cc0c5cc0f083
Tanel Põld <tanel@brightside.ee>
2023-03-21 09:46:41 +0200

    [TASK] Release 3.5.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'Personnel',
 	'description' => 'Personnel lists with vCard download.',
 	'category' => 'fe',
-	'version' => '3.4.1',
+	'version' => '3.5.0',
 	'state' => 'stable',
 	'clearcacheonload' => true,
 	'author' => 'Tanel Põld, Nikolay Orlenko',
@@ -13,7 +13,8 @@ $EM_CONF[$_EXTKEY] = [
 		'depends' => [
 			'typo3' => '11.5.0 - 12.99.99',
 			'fluid_styled_content' => '11.5.0 - 12.99.99',
-			'paginatedprocessors' => '1.4.2 - 1.99.99',
+			'paginatedprocessors' => '1.5.0 - 1.99.99',
+            'embedassets' => '1.2.0-1.99.99',
 		],
 	],
 	'autoload' => [

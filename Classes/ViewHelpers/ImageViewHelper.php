ef6d6ebb197ff8fa98b5b786f3745883e28fb09a
Tanel Põld <tanel@brightside.ee>
2024-01-18 11:18:21 +0200

    [TASK] Replace file_get_contents() with getContents() for vCard images

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -1,68 +1,16 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Brightside\Personnel\ViewHelpers;
-
-
-/*                                                                        *
- * This script is part of the TYPO3 project - inspiring people to share!  *
- *                                                                        *
- * TYPO3 is free software; you can redistribute it and/or modify it under *
- * the terms of the GNU General Public License version 2 as published by  *
- * the Free Software Foundation.                                          *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
- * Public License for more details.                                       *
- *                                                                        */
 
 use TYPO3\CMS\Core\Imaging\ImageManipulation\CropVariantCollection;
 use TYPO3\CMS\Core\Resource\Exception\ResourceDoesNotExistException;
+use TYPO3\CMS\Extbase\Service\ImageService;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Exception;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
 
-/**
- * Resizes a given image (if required) and renders the respective img tag
- *
- * = Examples =
- *
- * <code title="Default">
- * <f:image src="EXT:myext/Resources/Public/typo3_logo.png" alt="alt text" />
- * </code>
- * <output>
- * <img alt="alt text" src="typo3conf/ext/myext/Resources/Public/typo3_logo.png" width="396" height="375" />
- * or (in BE mode):
- * <img alt="alt text" src="../typo3conf/ext/viewhelpertest/Resources/Public/typo3_logo.png" width="396" height="375" />
- * </output>
- *
- * <code title="Image Object">
- * <f:image image="{imageObject}" />
- * </code>
- * <output>
- * <img alt="alt set in image record" src="fileadmin/_processed_/323223424.png" width="396" height="375" />
- * </output>
- *
- * <code title="Inline notation">
- * {f:image(src: 'EXT:viewhelpertest/Resources/Public/typo3_logo.png', alt: 'alt text', minWidth: 30, maxWidth: 40)}
- * </code>
- * <output>
- * <img alt="alt text" src="../typo3temp/assets/images/f13d79a526.png" width="40" height="38" />
- * (depending on your TYPO3s encryption key)
- * </output>
- *
- * <code title="Other resource type (e.g. PDF)">
- * <f:image src="fileadmin/user_upload/example.pdf" alt="foo" />
- * </code>
- * <output>
- * If your graphics processing library is set up correctly then it will output a thumbnail of the first page of your PDF document.
- * <img src="fileadmin/_processed_/1/2/csm_example_aabbcc112233.gif" width="200" height="284" alt="foo">
- * </output>
- *
- * <code title="Non-existent image">
- * <f:image src="NonExistingImage.png" alt="foo" />
- * </code>
- * <output>
- * Could not get image resource for "NonExistingImage.png".
- * </output>
- */
-class ImageViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper
+class ImageViewHelper extends AbstractTagBasedViewHelper
 {
     /**
      * @var string
@@ -77,7 +25,7 @@ class ImageViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBased
     /**
      * @param \TYPO3\CMS\Extbase\Service\ImageService $imageService
      */
-    public function injectImageService(\TYPO3\CMS\Extbase\Service\ImageService $imageService)
+    public function injectImageService(ImageService $imageService)
     {
         $this->imageService = $imageService;
     }
@@ -120,7 +68,7 @@ class ImageViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBased
     public function render()
     {
         if ((is_null($this->arguments['src']) && is_null($this->arguments['image'])) || (!is_null($this->arguments['src']) && !is_null($this->arguments['image']))) {
-            throw new \TYPO3\CMS\Fluid\Core\ViewHelper\Exception('You must either specify a string src or a File object.', 1382284106);
+            throw new Exception('You must either specify a string src or a File object.', 1382284106);
         }
 
         try {
@@ -151,14 +99,10 @@ class ImageViewHelper extends \TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBased
                     $this->tag->addAttribute('data-focus-area', $focusArea->makeAbsoluteBasedOnFile($image));
                 }
             }
-            $this->tag->addAttribute('src', $imageUri);
-               # $root = explode("/",realpath($_SERVER['DOCUMENT_ROOT']));
-               # unset($root[count($root) - 1]);
-               # $root = implode("/", $root);
             $type = pathinfo($imageUri, PATHINFO_EXTENSION);
-            $data = file_get_contents($imageUri);
+//            $data = file_get_contents($imageUri); - old way
+            $data = $processedImage->getContents();
             $imageUri = $type . ';ENCODING=BASE64:' . base64_encode($data);
-            // alternatively specify an URL, if PHP settings allow
 
             $this->tag->addAttribute('width', $processedImage->getProperty('width'));
             $this->tag->addAttribute('height', $processedImage->getProperty('height'));

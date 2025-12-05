7c7b6dc52e47d356986bcf273d579594baa0e825
Tanel Põld <tanel@brightside.ee>
2025-12-05 18:55:44 +0200

    [TASK] upgrade to v14 compatibility

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -6,126 +6,157 @@ namespace Brightside\Personnel\ViewHelpers;
 
 use TYPO3\CMS\Core\Imaging\ImageManipulation\CropVariantCollection;
 use TYPO3\CMS\Core\Resource\Exception\ResourceDoesNotExistException;
-use TYPO3\CMS\Extbase\Service\ImageService;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Resource\File as FalFile;
+use TYPO3\CMS\Core\Resource\ProcessedFile;
+use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Fluid\Core\ViewHelper\Exception;
-use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper; // <<-- CHANGED BASE CLASS
+use TYPO3\CMS\Extbase\Service\ImageService;
+use TYPO3\CMS\Core\Resource\FileInterface;
 
-class ImageViewHelper extends AbstractTagBasedViewHelper
+// NOTE: We no longer extend AbstractTagBasedViewHelper
+class ImageViewHelper extends AbstractViewHelper
 {
     /**
-     * @var string
+     * @var ResourceFactory
      */
-    protected $tagName = 'img';
+    protected ResourceFactory $resourceFactory;
 
     /**
-     * @var \TYPO3\CMS\Extbase\Service\ImageService
+     * @var ImageService
      */
-    protected $imageService;
+    protected ImageService $imageService;
+
+    // We do not need $tagName anymore.
 
     /**
-     * @param \TYPO3\CMS\Extbase\Service\ImageService $imageService
+     * Inject ResourceFactory.
      */
-    public function injectImageService(ImageService $imageService)
+    public function injectResourceFactory(ResourceFactory $resourceFactory): void
     {
-        $this->imageService = $imageService;
+        $this->resourceFactory = $resourceFactory;
+    }
+
+    /**
+     * Inject ImageService (TYPO3\CMS\Extbase\Service\ImageService).
+     */
+    public function injectImageService(ImageService $imageService): void
+    {
+        $this->imageService = $imageService; 
     }
 
     /**
      * Initialize arguments.
+     * if we want them, since AbstractViewHelper doesn't handle tags.
      */
-    public function initializeArguments()
+    public function initializeArguments(): void
     {
-        parent::initializeArguments();
-        $this->registerUniversalTagAttributes();
-        $this->registerTagAttribute('alt', 'string', 'Specifies an alternate text for an image', false);
-        $this->registerTagAttribute('ismap', 'string', 'Specifies an image as a server-side image-map. Rarely used. Look at usemap instead', false);
-        $this->registerTagAttribute('longdesc', 'string', 'Specifies the URL to a document that contains a long description of an image', false);
-        $this->registerTagAttribute('usemap', 'string', 'Specifies an image as a client-side image-map', false);
-
         $this->registerArgument('src', 'string', 'a path to a file, a combined FAL identifier or an uid (int). If $treatIdAsReference is set, the integer is considered the uid of the sys_file_reference record. If you already got a FAL object, consider using the $image parameter instead');
-        $this->registerArgument('treatIdAsReference', 'bool', 'given src argument is a sys_file_reference record');
+        $this->registerArgument('treatIdAsReference', 'bool', 'given src argument is a sys_file_reference record', false, false);
         $this->registerArgument('image', 'object', 'a FAL object');
         $this->registerArgument('crop', 'string|bool', 'overrule cropping of image (setting to FALSE disables the cropping set in FileReference)');
         $this->registerArgument('cropVariant', 'string', 'select a cropping variant, in case multiple croppings have been specified or stored in FileReference', false, 'default');
-
-        $this->registerArgument('width', 'string', 'width of the image. This can be a numeric value representing the fixed width of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.');
-        $this->registerArgument('height', 'string', 'height of the image. This can be a numeric value representing the fixed height of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.');
+        $this->registerArgument('width', 'string', 'width of the image.');
+        $this->registerArgument('height', 'string', 'height of the image.');
         $this->registerArgument('minWidth', 'int', 'minimum width of the image');
         $this->registerArgument('minHeight', 'int', 'minimum width of the image');
         $this->registerArgument('maxWidth', 'int', 'minimum width of the image');
         $this->registerArgument('maxHeight', 'int', 'minimum width of the image');
         $this->registerArgument('absolute', 'bool', 'Force absolute URL', false, false);
+        $this->registerArgument('alt', 'string', 'Alternative text for the image.', false, '');
+        $this->registerArgument('title', 'string', 'Title text for the image.', false, '');
     }
 
     /**
-     * Resizes a given image (if required) and renders the respective img tag
-     *
-     * @see https://docs.typo3.org/typo3cms/TyposcriptReference/ContentObjects/Image/
-     *
-     * @throws \TYPO3\CMS\Fluid\Core\ViewHelper\Exception
-     * @return string Rendered tag
+     * Renders the image data as a vCard PHOTO property string.
      */
-    public function render()
+    public function render(): string
     {
         if ((is_null($this->arguments['src']) && is_null($this->arguments['image'])) || (!is_null($this->arguments['src']) && !is_null($this->arguments['image']))) {
             throw new Exception('You must either specify a string src or a File object.', 1382284106);
         }
 
         try {
-            $image = $this->imageService->getImage((string)$this->arguments['src'], $this->arguments['image'], (bool)$this->arguments['treatIdAsReference']);
-            $cropString = $this->arguments['crop'];
-            if ($cropString === null && $image->hasProperty('crop') && $image->getProperty('crop')) {
-                $cropString = $image->getProperty('crop');
+            // --- 1. Get the FAL File Object ---
+            /** @var FileInterface|FileReference $image */
+            $image = $this->getFileObject();
+            if (!$image instanceof FileInterface && !$image instanceof FileReference) {
+                return '';
             }
+
+            // --- 2. Define Cropping & Processing ---
+            $cropString = $this->arguments['crop'] ?? ($image instanceof FileReference ? $image->getProperty('crop') : null);
+
             $cropVariantCollection = CropVariantCollection::create((string)$cropString);
             $cropVariant = $this->arguments['cropVariant'] ?: 'default';
             $cropArea = $cropVariantCollection->getCropArea($cropVariant);
+            
+            $cropArray = null;
+            if (!$cropArea->isEmpty()) {
+                $cropAreaObject = $cropArea->makeAbsoluteBasedOnFile($image);
+                $cropArray = method_exists($cropAreaObject, 'toArray') ? $cropAreaObject->toArray() : null;
+            }
+
             $processingInstructions = [
-                'absolute' => $this->arguments['absolute'],
                 'width' => $this->arguments['width'],
                 'height' => $this->arguments['height'],
                 'minWidth' => $this->arguments['minWidth'],
                 'minHeight' => $this->arguments['minHeight'],
                 'maxWidth' => $this->arguments['maxWidth'],
                 'maxHeight' => $this->arguments['maxHeight'],
-                'crop' => $cropArea->isEmpty() ? null : $cropArea->makeAbsoluteBasedOnFile($image),
+                'crop' => $cropArray, 
+                'absolute' => (bool)$this->arguments['absolute'],
             ];
+            
+            // --- 3. Process the Image ---
+            /** @var ProcessedFile $processedImage */
             $processedImage = $this->imageService->applyProcessingInstructions($image, $processingInstructions);
-            $imageUri = $this->imageService->getImageUri($processedImage, $this->arguments['absolute']);
+            
+            // --- 4. Get Data and Mime Type ---
+            $mimeType = $processedImage->getMimeType();
+            $extension = pathinfo($processedImage->getName(), PATHINFO_EXTENSION);
+            
+            // vCard specification typically prefers JPEG/JPG or PNG
+            $vCardMime = match ($mimeType) {
+                'image/jpeg' => 'jpg',
+                'image/png' => 'png',
+                default => 'jpeg', // Fallback
+            };
 
-            if (!$this->tag->hasAttribute('data-focus-area')) {
-                $focusArea = $cropVariantCollection->getFocusArea($cropVariant);
-                if (!$focusArea->isEmpty()) {
-                    $this->tag->addAttribute('data-focus-area', $focusArea->makeAbsoluteBasedOnFile($image));
-                }
-            }
-            $type = pathinfo($imageUri, PATHINFO_EXTENSION);
-//            $data = file_get_contents($imageUri); - old way
             $data = $processedImage->getContents();
-            $imageUri = $type . ';ENCODING=BASE64:' . base64_encode($data);
-
-            $this->tag->addAttribute('width', $processedImage->getProperty('width'));
-            $this->tag->addAttribute('height', $processedImage->getProperty('height'));
-
-            $alt = $image->getProperty('alternative');
-            $title = $image->getProperty('title');
-
-            // The alt-attribute is mandatory to have valid html-code, therefore add it even if it is empty
-            if (empty($this->arguments['alt'])) {
-                $this->tag->addAttribute('alt', $alt);
-            }
-            if (empty($this->arguments['title']) && $title) {
-                $this->tag->addAttribute('title', $title);
-            }
-        } catch (ResourceDoesNotExistException $e) {
-            // thrown if file does not exist
-        } catch (\UnexpectedValueException $e) {
-            // thrown if a file has been replaced with a folder
-        } catch (\RuntimeException $e) {
-            // RuntimeException thrown if a file is outside of a storage
-        } catch (\InvalidArgumentException $e) {
-            // thrown if file storage does not exist
+            $base64Data = base64_encode($data);
+            
+            // --- 5. Return vCard PHOTO String ---
+            $vCardString = 'PHOTO;' . strtoupper($vCardMime) . ';ENCODING=BASE64:' . $base64Data;
+            
+            return $vCardString;
+            
+        } catch (ResourceDoesNotExistException | \UnexpectedValueException | \RuntimeException | \InvalidArgumentException $e) {
         }
-        return $imageUri;
+        
+        return '';
+    }
+
+    /**
+     * @return FileReference|FalFile|null
+     * @throws Exception
+     */
+    protected function getFileObject(): FileReference|FalFile|null
+    {
+        if ($this->arguments['image'] !== null) {
+            return $this->arguments['image'];
+        }
+        
+        $src = (string)$this->arguments['src'];
+        if ((bool)$this->arguments['treatIdAsReference'] === true) {
+            return $this->resourceFactory->getFileReferenceObject($src);
+        }
+
+        if (is_numeric($src) && (int)$src > 0) {
+            return $this->resourceFactory->getFileObject($src);
+        }
+
+        return $this->resourceFactory->retrieveFileOrUsePublicUrl($src);
     }
 }
